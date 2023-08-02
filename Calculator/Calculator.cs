cb1a35ae89d0de075ec340c1f5b469409935f083
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-02 17:44:43 +0300

    Add Task 5

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+	internal class Calculator
+	{
+		public static dynamic Add(dynamic x, dynamic y) => x + y;
+		public static dynamic Subtract(dynamic x, dynamic y) => x - y;
+		public static dynamic Multiple(dynamic x, dynamic y) => x * y;
+		public static dynamic Divide(dynamic x, dynamic y) => y != 0 ? x / y : throw new DivideByZeroException();
+
+	}
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Calculator
+﻿namespace Task5
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+            Console.WriteLine(Calculator.Add(2.4, 2));
+            Console.WriteLine(Calculator.Subtract(4, 0));
+            Console.WriteLine(Calculator.Multiple(2.4f, 2.2));
+            Console.WriteLine(Calculator.Divide(8, 2));
+
+        }
 	}
 }
